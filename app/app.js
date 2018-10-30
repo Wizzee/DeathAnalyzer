968cbe80f6d028cff924a1775e9b511af2d7999d
Shane Vroegop <wizzy0309@hotmail.com>
2018-10-30 22:29:17 +1000

    basics

--- app/app.js
+++ app/app.js
@@ -3,6 +3,8 @@ var app = express();
 
 var port = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
     res.send('<html><head></head><body><h1>Death Analyzer</h1></body></html>');
 });
