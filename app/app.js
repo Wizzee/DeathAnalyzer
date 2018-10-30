eb78190819188dfc07905bb8a898dbb4592dad2e
Shane Vroegop <wizzy0309@hotmail.com>
2018-10-30 22:31:20 +1000

    Setting up template

--- app/app.js
+++ app/app.js
@@ -6,7 +6,7 @@ var port = process.env.PORT || 3000;
 app.set('view engine', 'ejs');
 
 app.get('/', function(req, res) {
-    res.send('<html><head></head><body><h1>Death Analyzer</h1></body></html>');
+    res.render('index');
 });
 
 app.get('/:region/:id', function(req, res) {
