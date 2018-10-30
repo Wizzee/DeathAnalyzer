26349a1e47aff6a94eda18071cc87da393dbc214
Shane Vroegop <wizzy0309@hotmail.com>
2018-10-30 22:05:28 +1000

    Initial setup

---
+++ app/app.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var app = express();
+
+var port = process.env.PORT || 3000;
+
+app.get('/', function(req, res) {
+    res.send('<html><head></head><body><h1>Death Analyzer</h1></body></html>');
+});
+
+app.get('/:region/:id', function(req, res) {
+    res.send(`<html><head></head><body><h1>Looking at ${req.params.id}'s profile in the ${req.params.region} region</h1></body></html>`);
+});
+
+app.listen(port);
